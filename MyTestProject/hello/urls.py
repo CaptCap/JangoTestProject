0298a4bbf2c250e4b77222d6a12377a5ed87c4ac
CaptCap <112699343+CaptCap@users.noreply.github.com>
2025-04-26 01:54:14 +0300

    greet function added

--- MyTestProject/hello/urls.py
+++ MyTestProject/hello/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.wes, name="wes" )
+    path("<str:name>", views.greet, name="greet"),
+    path("wes/", views.wes, name="wes" )
 ]
--- MyTestProject/hello/views.py
+++ MyTestProject/hello/views.py
@@ -6,3 +6,6 @@ def index(requests):
     return HttpResponse("Hello, Moto")
 def wes(request):
     return HttpResponse("Hello, Wes!")
+
+def greet(requests, name):
+    return HttpResponse(f"Hello, {name}")
