e083812ae1bfbf1c404088f7c01f36b33e13fc6a
CaptCap <112699343+CaptCap@users.noreply.github.com>
2025-04-26 02:08:31 +0300

    first template added

--- MyTestProject/hello/views.py
+++ MyTestProject/hello/views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(requests):
-    return HttpResponse("Hello, Moto")
+def index(request):
+    return render(request, "hello/index.html")
 def wes(request):
     return HttpResponse("Hello, Wes!")
 
-def greet(requests, name):
+def greet(request, name):
     return HttpResponse(f"Hello, {name}")
